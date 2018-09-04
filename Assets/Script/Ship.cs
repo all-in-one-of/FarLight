dd061d80a15f11b19d6babfb9a19762904838d01
HeliSRaiN <lhelisrainl@gmail.com>
2018-09-04 21:23:13 +0300

    FirstCommit

---
+++ Assets/Script/Ship.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour {
+    public string Name = null;
+    public float MaxSpeed = 0;
+    public float  Acceleration = 0;
+    public float FuelCapacity = 0;
+
+}
